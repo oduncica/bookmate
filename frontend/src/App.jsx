a83790ab54b71eb4e67989867b6b830739d2980d
agathemrnt <agathemf@hotmail.fr>
2025-04-02 18:14:02 +0200

    Debugging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage";
@@ -10,6 +10,7 @@ import TermsPage from "./pages/TermsPage";
 import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
 import PrivacyPolicy from "./components/PrivacyPolicy";
+
 import IgnoredBooks from "./components/IgnoredBooks";
 import AdvancedSearch from "./components/AvancedSearch";
 import Questionnaire from "./components/Questionnaire";
@@ -19,22 +20,16 @@ import { useAuthStore } from "./store/useAuthStore";
 
 function App() {
   const { checkAuth, authUser, checkingAuth } = useAuthStore();
-  const location = useLocation();
-  const [showNavbar, setShowNavbar] = useState(true);
 
   useEffect(() => {
-    // Liste des pages où la navbar doit être cachée
-    const hiddenNavbarRoutes = ["/questionnaire"];
-    
-    // Vérifie si l'URL actuelle est dans la liste et met à jour showNavbar
-    setShowNavbar(!hiddenNavbarRoutes.includes(location.pathname));
-  }, [location.pathname]);
+    checkAuth();
+  }, [checkAuth]);
 
   if (checkingAuth) return null;
 
   return (
     <div className="min-h-screen">
-      {authUser && showNavbar && <Navbar />}
+      {authUser && <Navbar />}
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
@@ -67,8 +62,14 @@ function App() {
             path="/search"
             element={authUser ? <AdvancedSearch /> : <Navigate to="/auth" />}
           />
-          <Route path="/terms" element={authUser ? <TermsPage /> : <Navigate to="/auth" />} />
-          <Route path="/privacy" element={authUser ? <PrivacyPolicy /> : <Navigate to="/auth"/>} />
+          <Route 
+            path="/terms" 
+            element={authUser ? <TermsPage /> : <Navigate to="/auth" />}
+          />
+          <Route 
+          path="/privacy"
+          element={authUser ? <PrivacyPolicy /> : <Navigate to="/auth"/>} 
+          />
           <Route path="/passwordreset" element={<ForgotPassword />} />
           <Route path="/passwordreset/:token" element={<ResetPassword />} />
         </Routes>
